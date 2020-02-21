0e781b1d46388a8193870b1f55751099c88720be
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2020-02-21 15:41:54 +0530

    more minor fixes.

--- src/main/scala/CryptArithmetic.scala
+++ src/main/scala/CryptArithmetic.scala
@@ -3,9 +3,14 @@ package midterm
 import com.microsoft.z3
 
 object CryptArithmetic {
-  case class Puzzle(wordA : String, wordB : String, mapping : Map[Char, Int])
+  case class Puzzle(wordA : String, wordB : String, wordC : String, mapping : Map[Char, Int]) {
+    override def toString() = 
+      wordA + " + " + wordB + " = " + wordC + "; " + 
+      mapping.map{ case (k, v) => k.toString + "->" + v.toString }.mkString("", ", ", "")
+  }
 
-  def findLongestPuzzle() : Puzzle = {
-    throw new NotImplementedError("Implement findLongestPuzzle.")
+  /** Return a valid cryptarithmetic puzzle with length at least minLen. */
+  def findPuzzle(minLen : Int) : Puzzle = {
+    throw new NotImplementedError("Implement findPuzzle.")
   }
 }
